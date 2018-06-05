072cf512d933ec11bf5f746eb358eb9f12471b9b
LuK1337 <priv.luk@gmail.com>
2018-06-05 12:04:07 +0200

    Use https

--- sbot.py
+++ sbot.py
@@ -1,7 +1,7 @@
 class StrawPoll:
     import requests, lxml.html, json
 
-    url = "http://www.strawpoll.me"
+    url = "https://www.strawpoll.me"
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest"
